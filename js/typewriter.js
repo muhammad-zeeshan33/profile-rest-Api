1c9f93a60b375451e230f3932063291f689230d7
Muhammad Zeeshan <61107542+web-tech33@users.noreply.github.com>
2021-04-29 00:48:09 -0700

    Add files via upload

---
+++ js/typewriter.js
@@ -0,0 +1,22 @@
+var typed = new Typed('.typewriter', {
+    strings: [
+       
+      'The UI/UX Designer',
+      'The Programmer',
+      'The Web Designer',
+      'The Developer'     
+    ],
+    typeSpeed: 120,
+    backSpeed: 120,
+    loop: true
+    
+  });
+  var typed = new Typed('.element1',{
+    strings:  [
+      'Welcome to the world of ZeeWebist'
+    ],
+    typeSpeed: 120,
+    backSpeed: 0,
+    loop: false
+  })
+  
